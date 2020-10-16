d07c96acdcf5fcd704bca7e2823def18b054015d
Paul Holden <paul@codelunker.com>
2020-10-16 13:08:44 -0700

    refactor: Remove exhaustive requires

--- lib/taleo.rb
+++ lib/taleo.rb
@@ -1,11 +1,7 @@
 require 'json'
 require 'faraday'
 require 'taleo/version'
-require 'taleo/resource'
 require 'taleo/client'
-require 'taleo/candidate'
-require 'taleo/employee'
-require 'taleo/packet'
 
 module Taleo
   class Error < StandardError; end
